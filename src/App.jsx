cc9a9b5bb7688c78a5ce97f798bc468eecc97ace
Rohan Puri <purirohan62@gmail.com>
2025-11-27 13:14:52 -0500

    add WeatherApp component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import WeatherApp from './WeatherApp'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='flex justify-center items-center min-h-screen font-mono'>
+      <WeatherApp />
+    </div>
   )
 }
 
